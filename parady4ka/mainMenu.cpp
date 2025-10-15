6ce074ee2ef21ab0657f92ebc4f038aa48536987
Платон Фіняк <dponomer52@gmail.com>
2025-10-15 18:30:13 +0300

    створено головне меню, в якому передбачені переходи до меню викладача та учня, а також вихід з програми.

---
+++ parady4ka/mainMenu.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void mainMenu()
+{
+    cout << "Welcome to ProStudentHelper!\nTo continue please choose one of the following options:" << endl;
+    cout << "1 - Continue as a student\n2 - Continue as a teacher\n0 - Quit the program" << endl;
+    int roleChoice;
+    cin >> roleChoice;
+    switch(roleChoice)
+    {
+        case 1:
+            //перехід у меню студента
+        case 2:
+            //перехід у меню викладача
+        case 0:
+            abort();
+        default:
+            cout << "Please, choose an apropriate option." << endl;
+            mainMenu();
+    }
+    
+}
