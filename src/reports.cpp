899246bb64f8b8768a4292d9d725c72da1fe9885
anastasiiayerchenko <anastasiia.yerchenko.shi.2025@lpnu.ua>
2025-10-16 18:40:37 +0300

    feature/expulsion_list_final

--- src/reports.cpp
+++ src/reports.cpp
@@ -5,26 +5,13 @@ using namespace std;
 
 extern StudentDatabase db;
 
-float calculateStudentAverage(const Student& student) {
-    int totalScore = 0;
-    int scoreCount = 0;
-    for (const auto& subject : student.subjects) {
-        for (int score : subject.scores) {
-            totalScore += score;
-            scoreCount++;
-        }
-    }
-    if (scoreCount == 0) {
-        return 0.0f;
-    }
-    return static_cast<float>(totalScore) / scoreCount;
-}
+double calculateAverageScore(const std::vector<Subject>& subjects);
 
 void printExpulsionList() {
     vector<Student> expulsion_candidates;
 
     for (const auto& student : db.getAllStudents()) {
-        float avgScore = calculateStudentAverage(student);
+        double avgScore = calculateAverageScore(student.subjects);
         if (avgScore > 0 && avgScore < 50.0f) {
             expulsion_candidates.push_back(student);
         }
@@ -36,7 +23,7 @@ void printExpulsionList() {
     } else {
         for (const auto& student : expulsion_candidates) {
              cout << "  - Name: " << student.name
-                      << ", Average Score: " << calculateStudentAverage(student) << endl;
+                      << ", Average Score: " << calculateAverageScore(student.subjects) << endl;
         }
     }
  cout << "-------------------------------------------\n";
