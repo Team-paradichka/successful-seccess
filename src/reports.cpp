0c1a32059bbf5ae2556312e200a5a6f57347fe60
Maksym Hrechanyi <maksym.hrechanyi.shi.2025@lpnu.ua>
2025-10-16 20:22:38 +0300

    Deleted unnecessary lines

--- src/reports.cpp
+++ src/reports.cpp
@@ -6,8 +6,6 @@ void printHonorStudents(){
     cout << "============ Honor Students ============" << endl;
 
     for(const auto& student : db.getAllStudents()){
-        double total_sum = 0.0;
-        double subject_count = 0;
         double average_score = calculateAverageScore(const std::vector<Subject>& subjects);
 
         if (average_score >= 88.0){
