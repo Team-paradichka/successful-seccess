d82f44002171f063f57e86289922ec12d4f80321
Платон Фіняк <dponomer52@gmail.com>
2025-10-16 19:51:33 +0300

    the FINAL version of the main menu code

--- parady4ka/mainMenu.cpp
+++ parady4ka/mainMenu.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void main_menu()
+void mainMenu()
 {
     cout << "Welcome to ProStudentHelper!\nTo continue please choose one of the following options:" << endl;
     cout << "1 - Continue as a student\n2 - Continue as a teacher\n0 - Quit the program" << endl;
@@ -19,7 +19,7 @@ void main_menu()
             abort();
         default:
             cout << "Please, choose an apropriate option." << endl;
-            main_menu();
+            mainMenu();
     }
     
 }
