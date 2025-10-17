90b40d8ec1f29bac3020112b812c45dd59d3b0a9
markiianpitulashi2025-ai <markiian.pitula.shi.2025@lpnu.ua>
2025-10-17 10:19:11 +0300

    feat: Implement UI of a teacher

--- src/ui.cpp
+++ src/ui.cpp
@@ -8,16 +8,18 @@ void showTeacherMenu() {
 
     while (true) {
         cout << "\n===== Teacher Main Menu =====" << endl;
-        cout << "1. Вивести оцінки студента" << endl;
-        cout << "2. Вивести список усіх зареєстрованих студентів" << endl;
-        cout << "3. Додати нового студента" << endl;
-        cout << "4. Знайти найкращого та найгіршого студента (за середнім балом)" << endl;
-        cout << "5. Показати середній бал усієї групи" << endl;
-        cout << "6. Показати список студентів, які отримують стипендію (avg > 70)" << endl;
-        cout << "7. Порахувати кількість відмінників (avg > 88)" << endl;
-        cout << "8. Вивести список студентів на відрахування (avg < 50)" << endl;
-        cout << "0. Вийти до головного меню" << endl;
-        cout << "Ваш вибір: ";
+        cout << "1. Display a student's grades" << endl;
+        cout << "2. Show the list of all registered students" << endl;
+        cout << "3. Add a new student" << endl;
+        cout << "4. Find the best and worst student (by average grade)" << endl;
+        cout << "5. Show a student's average grade" << endl;
+        cout << "6. Show the group's average grade" << endl;
+        cout << "7. Show the list of students receiving a scholarship (avg > 70)" << endl;
+        cout << "8. Count the number of excellent students (avg > 88)" << endl;
+        cout << "9. Show the list of students subject to expulsion (avg < 50)" << endl;
+        cout << "0. Return to the main menu" << endl;
+        cout << "Your choice: ";
+
         cin >> choice;
 
         switch (choice) {
@@ -28,28 +30,31 @@ void showTeacherMenu() {
             printAllStudents();
             break;
         case 3:
-            db.addNewStudent();               
+            addStudentMenu();               
             break;
         case 4:
             printBestAndWorstStudents();
             break;
         case 5:
+            printStudentAverage();
+            break; 
+        case 6:
             printGroupAverage();
             break;
-        case 6:
-            printScholarshipList();
-            break;
         case 7:
-            countExcellentStudents();
+            get_scholarship_student();
             break;
         case 8:
+            printHonorStudents();
+            break;
+        case 9:
             printExpulsionList();
             break;
         case 0:
-            cout << "Повернення до головного меню...\n";
+            mainMenu();
             return;
         default:
-            cout << "Невірний вибір. Спробуйте ще раз.\n";
+            cout << "Invalid choice. Try again\n";
             break;
         }
     }
